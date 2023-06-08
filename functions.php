40c83a9a62b1731c1f73badee0a0ad319fa6e9c0
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-06-08 05:13:33 -0700

    init

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/* Enqueue Styles */
+if ( ! function_exists('thr_enqueue_styles') ) {
+    function thr_enqueue_styles() {
+        wp_enqueue_style( 'twenty-twenty-three-style', get_template_directory_uri() .'/style.css' );
+    }
+    add_action('wp_enqueue_scripts', 'thr_enqueue_styles');
+}
+
+add_shortcode('featured_image', 'my_post_featured_img');
