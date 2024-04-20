9ed7a34d87e5b6c9e2fc0146fb9bf76c0f4c8712
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-04-19 17:00:33 -0700

    rm acf block code

--- functions.php
+++ functions.php
@@ -8,35 +8,3 @@ if ( ! function_exists('thr_enqueue_styles') ) {
 }
 
 add_shortcode('featured_image', 'my_post_featured_img');
-
-// Creating an ACF block
-// https://www.advancedcustomfields.com/blog/build-business-listing-acf-blocks-query-loop-block-theme/
-add_action( 'acf/init', 'lch_acf_init_blocks' );
-function lch_acf_init_blocks() {
-
-    if ( function_exists( 'acf_register_block_type' ) ) {
-        acf_register_block_type(
-            array(
-                'name'            => 'artillery-link',
-                'title'           => 'Artillery link',
-                'description'     => 'Add article artillery link.',
-                // This is the PHP file that is used to render the output for our block. 
-                // Inside this file, you can use any WordPress functions you could normally use in a template file.
-                'render_template' => 'block-templates/artillery-link.php',
-                'category'        => 'text',
-                'icon'            => 'admin-links',
-                'api_version'     => 2,
-                'keywords'        => array( 'artillery link', 'link' ),
-                'mode'            => 'preview',
-                'supports'        => array(
-                    'jsx'        => true,
-                    'color'      => array(
-                        'text'       => true,
-                        'background' => false,
-                    ),
-                    'align_text' => true,
-                ),
-            )
-        );
-    }
-}
